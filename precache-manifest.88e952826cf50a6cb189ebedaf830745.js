e35aa52a11859d4ac6f9fe7aa03c395aa158c8b0
namlh023 <hoainamle023@gmail.com>
2022-04-18 15:21:40 +0700

    Updates

---
+++ precache-manifest.88e952826cf50a6cb189ebedaf830745.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a8233be782c613778a014ada8b513df",
+    "url": "/react-ssr-example/index.html"
+  },
+  {
+    "revision": "06119fe1630ab696391e",
+    "url": "/react-ssr-example/static/css/main.9136cf8d.chunk.css"
+  },
+  {
+    "revision": "a05cc991f8d31d90a46b",
+    "url": "/react-ssr-example/static/js/2.b1e33f41.chunk.js"
+  },
+  {
+    "revision": "06119fe1630ab696391e",
+    "url": "/react-ssr-example/static/js/main.870dc993.chunk.js"
+  },
+  {
+    "revision": "4d337f043d1d4f7cfba6",
+    "url": "/react-ssr-example/static/js/runtime-main.19c246d0.js"
+  }
+]);
